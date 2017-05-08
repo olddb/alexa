5e11fdba16ab68a1c4cdedf6365d8e929ee0d3e9
olddb <vincentpailhe@gmail.com>
2017-05-08 18:22:54 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   credentials: {
-    clientID: "0e4165b1f0ed3a255c0939d03c373d0d86db4aedad09866c86509536bfbb1aee",
-    clientSecret: "52aa0e2652b74c513bd011a151c927ce6de17e9c924ad3b34f882bf4e3d975c5",
+    clientID: "",
+    clientSecret: "",
     site: 'https://api.intrav2.42.fr',
   }
 };
